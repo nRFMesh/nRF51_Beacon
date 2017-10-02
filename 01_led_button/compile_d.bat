3e9bc6bb02bc73ddc4392144f5bd6b1b156c41df
wassfila <wassim.filali@gmail.com>
2017-10-02 13:05:18 +0200

    added easy way to test the DONGLE

--- 01_led_button/compile_d.bat
+++ 01_led_button/compile_d.bat
@@ -1,1 +1,3 @@
 mbed compile -v --profile .\debug_nano.json
+
+;arm-none-eabi-objcopy -S -O binary .\BUILD\NRF51_DONGLE\GCC_ARM\01_led_button.elf .\BUILD\NRF51_DONGLE\GCC_ARM\01_led_button.bin
--- 01_led_button/flash.bat
+++ 01_led_button/flash.bat
@@ -1,2 +1,6 @@
 JLink.exe -device NRF51822_XXAA -If SWD -CommanderScript jlcmdfile.jlink
 
+;JLink.exe -device NRF51422_XXAC -If SWD -Speed 4000
+;loadfile .\BUILD\NRF51_DONGLE\GCC_ARM\01_led_button.hex,0x0001B000
+;loadfile .\mbed-os\features\FEATURE_BLE\targets\TARGET_NORDIC\TARGET_MCU_NRF51822\bootloader\s130_nrf51_1.0.0_bootloader.hex,0x00000000
+

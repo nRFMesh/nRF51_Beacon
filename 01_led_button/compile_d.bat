7acb606d6ab88720d3bc8334726257b224cdb3d8
wassfila <wassim.filali@gmail.com>
2017-10-02 18:52:42 +0200

    scripts and new os with less targets

--- 01_led_button/compile_d.bat
+++
@@ -1,3 +0,0 @@
-mbed compile -v --profile .\debug_nano.json
-
-;arm-none-eabi-objcopy -S -O binary .\BUILD\NRF51_DONGLE\GCC_ARM\01_led_button.elf .\BUILD\NRF51_DONGLE\GCC_ARM\01_led_button.bin
--- 01_led_button/main.cpp
+++ 01_led_button/main.cpp
@@ -1,10 +1,13 @@
 #include "mbed.h"
 
+#ifdef SOFTDEVICE_PRESENT
+#error Sof devices still here
+#endif
 
 void active_wait()
 {
   volatile uint32_t var = 0;
-  for (int i=0;i<100000;i++)
+  for (int i=0;i<300000;i++)
   {
     var++;
   }
@@ -19,7 +22,7 @@ int main()
     led = 0;      
     active_wait();
     led = 1;      
-    //TODO debug why this is not working wait_ms(500);   
+    //TODO debug why this is not working     wait_ms(500);   
     active_wait();
   }
 }
